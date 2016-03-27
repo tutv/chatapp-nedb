0551004b919b16d3ac888e9b59fda985ad91e3c3
Tu TV <tutv95@gmail.com>
2016-03-28 00:50:19 +0700

    Init jade

--- index.js
+++ index.js
@@ -1,41 +1,81 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var md5 = require('md5');
+var session = require('express-session');
+var mustacheExpress = require('mustache-express');
 
-var Datastore = require('nedb')
-	, db = new Datastore({filename: 'databases/chat.db', autoload: true});
+/**
+ * Static file
+ */
+app.use(express.static(__dirname + '/public'));
 
+/**
+ * Set engine template
+ */
+app.set('view engine', 'jade');
+
+app.set('views', __dirname + '/views');
+
+/**
+ * Databases
+ */
+var Datastore = require('nedb'),
+	messages_db = new Datastore({filename: 'databases/messages.db', autoload: true}),
+	users_db = new Datastore({filename: 'databases/users.db', autoload: true}),
+	rooms_db = new Datastore({filename: 'databases/rooms.db', autoload: true});
+
+/**
+ * Index
+ */
+messages_db.ensureIndex({fieldName: 'room_id'}, function (err) {
+	console.log('Error Indexing: ');
+	console.log(err);
+});
+
+/**
+ * Index
+ */
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
-//for (var i = 0; i < 1000000; i++) {
-//	db.insert([
-//		{
-//			msg : Math.random().toString(36).substring(7),
-//			type: 'message'
-//		}
-//	], function (err, newDocs) {
-//		console.log(newDocs);
-//		// Two documents were inserted in the database
-//		// newDocs is an array with these documents, augmented with their _id
-//	});
-//}
+app.get('/room/:id', function (req, res) {
+	res.render('room', {title: 'Hey', message: 'Hello there!'});
+});
 
+app.get('/room', function (req, res) {
+	res.render('rooms', {title: 'Rooms'});
+});
+
+/**
+ * API GET Message
+ */
 app.get('/get', function (req, res) {
-	db.find({}).sort({timestamp: 1}).limit(100).exec(function (err, docs) {
+	messages_db.find({}).sort({timestamp: 1}).limit(100).exec(function (err, docs) {
 		res.json(docs);
 	});
 });
 
+/**
+ * Seeder
+ */
+app.get('/seed', function (req, res) {
+	res.send('Hello!');
+});
+
+/**
+ * Socket
+ */
 io.on('connection', function (socket) {
-	console.log('a user connected');
+	console.log('A user connected');
 
 	socket.on('chat_message', function (msg) {
 		console.log('message: ' + msg);
 		var d = new Date();
 
-		db.insert([
+		messages_db.insert([
 			{
 				msg      : msg,
 				type     : 'message',
